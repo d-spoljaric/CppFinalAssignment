dd72d141fe70a469a8b6d7fdde763e60e0e62f48
Dominik Špoljarić <d.spoljaric@student.tudelft.nl>
2023-12-20 11:16:33 +0100

    Copied Weblab code into main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
+#include <cmath>
 #include <iostream>
+#include <initializer_list>
+#include <memory>
+#include <map>
+#include <stdexcept>
+#include <utility>
 
+template <typename T>
+class Vector{
+    // Your implementation of the Vector class starts here
+};
 
-int main(){
+template<typename T, typename U>
+typename std::common_type<T,U>::type dot(const Vector<T>& lhs, const Vector<U>& rhs){
+    // Your implementation of the dot function starts here
+}
 
-    std::cout << "Hello World!" << std::endl;
+template <typename T>
+class Matrix{
+    // Start your implementation of the matrix class here
+};
+
+template<typename T>
+int cg(const Matrix<T>& A,
+       const Vector<T>& b,
+       Vector<T>&       x,
+       T                tol     = (T)1e-8,
+       int              maxiter = 100){
+    // Your implementation of the cg function starts here
+}
+
+template <int n, typename T>
+class Heat{
+    // Your implementation of the heat class starts here
+};
+
+int main(int argc, char* argv[]){
+    // Your testing of the Heat class starts here
     return 0;
 }
